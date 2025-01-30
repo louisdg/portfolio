7ff2bfe0d963c620d0faabe1a985090feab3f193
Louis <36234057+louisdg@users.noreply.github.com>
2025-01-30 18:06:00 +0100

    Update work experience and resume (#7)

--- src/components/main-content/experience/Experience.tsx
+++ src/components/main-content/experience/Experience.tsx
@@ -6,18 +6,41 @@ function Experience() {
     <Stack id="experience" spacing={4}>
       <h2>Experience</h2>
       <WorkExperience
-        title="Full Stack Engineer"
+        title="Full Stack Software Engineer"
+        company="Origami Energy"
+        mainSkills={["React", "TypeScript", "MUI", "Node.js", "Express"]}
+        city="Cambridge"
+        country="United Kingdom"
+        startDate={new Date(2024, 7)}
+        tasks={[
+          "Designing modular, reusable, and scalable React components that integrate seamlessly across different parts of the application",
+          "Customising and theming the application using the MUI library to ensure a consistent, visually appealing design that aligns with brand guidelines",
+          "Writing and maintaining API endpoints in Node.js + Express for common features in the app, such as user information and notifications",
+          "Providing clear and thorough documentation for components, including usage guidelines, API details, and code examples, to support other development teams",
+          "Optimizing React components for performance, including lazy loading, code splitting, and memoization to enhance user experience",
+          "Managing application state using tools like Redux to ensure smooth data flow and application consistency",
+        ]}
+      />
+      <WorkExperience
+        title="Full Stack Software Engineer"
         company="Slate Digital"
-        mainSkills={["React", "TypeScript", "Node.js", "Kotlin", "PostgreSQL"]}
+        mainSkills={[
+          "React",
+          "TypeScript",
+          "Kotlin",
+          "Spring Boot",
+          "PostgreSQL",
+        ]}
         city="Grenoble"
         country="France"
         startDate={new Date(2021, 8)}
+        endDate={new Date(2024, 7)}
         tasks={[
-          "Designing and writing robust RESTful APIs in Kotlin + Spring Boot and translating visual mock-ups into React component-based user interfaces, styled with CSS + Sass",
+          "Designing and writing robust RESTful APIs in Kotlin + Spring Boot or Node.js + Express and translating visual mock-ups into React user interfaces",
           "Implementing authentication, authorization and payment mechanisms",
           "Data modeling, managing and interacting with PostgreSQL databases",
           "Developing Electron desktop applications",
-          "Implementing automated unit, end-to-end and integration tests with Jest, Vitest, TestCafe, Testing Library for the frontend, and JUnit for the backend",
+          "Implementing automated unit, end-to-end and integration tests with Jest, Vitest, TestCafe, Testing Library for JavaScript, and JUnit for Kotlin",
           "Using behaviour-driven development methodology to improve the design and maintainability of the software, especially the automated tests",
           "Refactoring codebases, reducing coupling of services, and rearchitecting and modernising technical stacks",
           "Setting up CI/CD pipelines to build, test and deploy applications",
